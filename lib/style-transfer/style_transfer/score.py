09304f3d645db2783b9e6ce8074ebcc6a813a000
Simon Meoni <simonmeoni@aol.com>
2024-02-07 14:17:29 +0100

    🐛 Style Transfer: resolve wandb bug when too much artifacts are uploaded

--- lib/style-transfer/style_transfer/score.py
+++ lib/style-transfer/style_transfer/score.py
@@ -8,20 +8,26 @@ import pandas as pd
 import torch
 import wandb
 from fastchat.conversation import get_conv_template
+from omegaconf import omegaconf
 from sentence_transformers import SentenceTransformer, util
 from style_transfer.utils import EVAL_PROMPT
 from tqdm import tqdm
-from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 @hydra.main(version_base="1.3", config_path="../configs", config_name="score.yaml")
 def main(cfg):
+    wandb.config = omegaconf.OmegaConf.to_container(
+        cfg,
+        resolve=True,
+        throw_on_missing=True,
+    )
     with wandb.init(project="score-style-transfer") as run:
         dataset = run.use_artifact(cfg.dataset)
         json_file = json.load(dataset.files()[0].download(replace=True))
         df = pd.DataFrame(data=json_file["data"], columns=json_file["columns"])
         dataset = datasets.Dataset.from_pandas(df)
 
+        logging.info("Loading the Semantic Model 🐈‍⬛")
         sem_model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
 
         def add_prompt(data_point):
@@ -49,24 +55,10 @@ def main(cfg):
             batch_size=cfg.batch_size,
         )
 
-        logging.info("Model + Tokenizer saved at models/merged/")
-        logging.info("Loading model to pipeline 🐉 ...")
-        model = AutoModelForCausalLM.from_pretrained(
-            pretrained_model_name_or_path=cfg.evaluator,
-            torch_dtype=torch.bfloat16,
-            device_map="auto",
-        )
-        model.save_pretrained("models/evaluator/")
-        tokenizer = AutoTokenizer.from_pretrained(cfg.evaluator)
-        tokenizer.save_pretrained("models/evaluator/")
-        del model
-        del tokenizer
-        pipe = mii.pipeline("models/evaluator/")
-        logging.info("Model loaded to pipeline ! 🎉")
-        logging.info("Loading the Semantic Model 🐈‍⬛")
+        client = mii.client("models/evaluator/") if cfg.use_g_score else None
 
         def g_scores(batch, seq):
-            responses = pipe(batch[f"eval_prompt_{seq}"], max_new_tokens=cfg.max_new_tokens)
+            responses = client(batch[f"eval_prompt_{seq}"], max_new_tokens=cfg.max_new_tokens)
             scores = [response.generated_text[-1] for response in responses]
             scores = [
                 float(score) if score.isdigit() and 0 <= float(score) <= 5 else 0
@@ -80,24 +72,27 @@ def main(cfg):
 
         def sem_scores(batch, seq):
             ground_enc = sem_model.encode(batch["ground_texts"])
-            prediction_enc = sem_model.encode(batch[f"eval_prompt_{seq}"])
+            prediction_enc = sem_model.encode(batch[f"generation_{seq}"])
             scores = [
                 util.cos_sim(ground_enc, pred_enc)[0][0].item()
                 for ground_enc, pred_enc in zip(ground_enc, prediction_enc)
             ]
             batch.setdefault(f"eval_sem_scores_{seq}", []).extend(scores)
 
-        new_dataset = []
+        dataset = []
         for batch in tqdm(dataloader):
             for seq in range(cfg.num_generated_sequences):
-                g_scores(batch, seq)
-                sem_scores(batch, seq)
+                if cfg.use_sem_score:
+                    sem_scores(batch, seq)
+                if cfg.use_g_score:
+                    g_scores(batch, seq)
 
-            new_dataset.extend([dict(zip(batch, t)) for t in zip(*batch.values())])
-            table = wandb.Table(dataframe=pd.DataFrame(batch))
-            wandb.log({"generation_predictions": table})
-        df = pd.DataFrame(new_dataset)
-        wandb.log({"dataframe_table": wandb.Table(dataframe=df)})
+            df = pd.DataFrame(batch)
+            dataset.append(df)
+
+        wandb.log({"score_dataset": wandb.Table(dataframe=pd.concat(dataset))})
+    if cfg.use_g_score:
+        client.terminate_server()
     wandb.finish()
 
 
