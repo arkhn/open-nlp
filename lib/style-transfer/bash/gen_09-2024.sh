adb8c12e5fe9ed47db7e690eda9834ea63261f7b
Simon Meoni <simonmeoni@aol.com>
2024-02-09 15:04:50 +0100

    ⚗️ Style Transfer: add experimentation scripts

---
+++ lib/style-transfer/bash/gen_09-2024.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ..
+python style_transfer/gen.py checkpoint=clinical-dream-team/sft-style-transfer/checkpoint-sft-ratio-0.99_gen-ratio-0.7:v281 sft_ratio=0.99 gen_ratio=0.7
+python style_transfer/gen.py checkpoint=clinical-dream-team/sft-style-transfer/checkpoint-sft-ratio-0.02_gen-ratio-0.7:v11 sft_ratio=0.02 gen_ratio=0.7
