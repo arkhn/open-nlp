a5a513bedaa0e894717d5fecbcd5bf20577281d4
Simon Meoni <simonmeoni@aol.com>
2024-07-24 15:50:19 +0200

    🗑️ Style Transfer: remove Bash script

--- lib/style-transfer/bash/score/14-02-2024.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python style_transfer/score.py \
-gen_dataset=clinical-dream-team/gen-style-transfer/run-qmopfb2e-gen_dataset:v0 \
-test_dataset=clinical-dream-team/gen-style-transfer/run-qmopfb2e-test_dataset:v0 \
-sft_ratio=0.006 \
-gen_ratio=0.7
