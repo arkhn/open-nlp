25853b0fec11cf118065b54329ad63de5ade09c1
Simon Meoni <simonmeoni@aol.com>
2024-07-24 15:50:19 +0200

    🗑️ Style Transfer: remove Bash script

--- lib/style-transfer/bash/gen/09-02-2024.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python style_transfer/gen.py checkpoint=clinical-dream-team/sft-style-transfer/checkpoint-sft-ratio-0.99_gen-ratio-0.7:v281 sft_ratio=0.99 gen_ratio=0.7 &&
-python style_transfer/gen.py checkpoint=clinical-dream-team/sft-style-transfer/checkpoint-sft-ratio-0.02_gen-ratio-0.7:v11 sft_ratio=0.02 gen_ratio=0.7
--- lib/style-transfer/bash/gen/21-02-2024.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python style_transfer/gen.py checkpoint=clinical-dream-team/dpo-style-transfer/checkpoint-rl7g2p97:v83 dpo_gen=1 sft_ratio=0.006 gen_ratio=0.7
