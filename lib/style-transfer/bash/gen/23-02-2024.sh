c6dfddd67df472c168d09c9b0149aa7f1410c1ac
Simon Meoni <simonmeoni@aol.com>
2024-07-24 15:50:19 +0200

    🗑️ Style Transfer: remove Bash script

--- lib/style-transfer/bash/gen/23-02-2024.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python style_transfer/gen.py checkpoint=clinical-dream-team/dpo-style-transfer/checkpoint-zy4fln6j:v37 dpo_gen=1 sft_ratio=0.06 gen_ratio=0.7
-python style_transfer/gen.py checkpoint=clinical-dream-team/sft-style-transfer/checkpoint-sft-ratio-0.04_gen-ratio-0.7:v13 dpo_gen=0 sft_ratio=0.04 gen_ratio=0.7
--- lib/style-transfer/bash/icd/16-04-2022.sh
+++
@@ -1,1 +0,0 @@
-python style_transfer/icd_train.py -m --config-name=ablation.yaml
