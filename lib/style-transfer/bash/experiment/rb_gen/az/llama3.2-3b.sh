e6ce3ec50db898a9dd50df64769782717f56bfc6
Simon Meoni <simonmeoni@aol.com>
2024-10-09 13:02:07 +0200

    🐛 Style Transfer: fix nccl error by wiping out vram

--- lib/style-transfer/bash/experiment/rb_gen/az/llama3.2-3b.sh
+++ lib/style-transfer/bash/experiment/rb_gen/az/llama3.2-3b.sh
@@ -5,4 +5,5 @@ python style_transfer/run_rb_gen.py model.name=meta-llama/Llama-3.2-3B-Instruct 
             dataset.num_generated_samples=500 \
             score.model.model_name_or_path=FremyCompany/BioLORD-2023-C \
             dataset.sft_ratio=0.06 \
-            dataset.gen_ratio=0.7
+            dataset.gen_ratio=0.7 \
+            sft.training_args.eval_steps=30
