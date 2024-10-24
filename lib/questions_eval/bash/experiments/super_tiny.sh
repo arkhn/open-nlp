ac306b2e843f9ac76184d6e4542618837631bf65
honghanhh <hanh.usth@gmail.com>
2024-10-24 14:28:41 +0200

    chore: remove comments

--- lib/questions_eval/bash/experiments/super_tiny.sh
+++ lib/questions_eval/bash/experiments/super_tiny.sh
@@ -1,3 +1,4 @@
 # python run.py -m model=gpt-4o-mini samples=10 num_questions=5
 # python run.py -m model=gpt-4o samples=10 num_questions=5
-python run.py -m model=llama3.1-405b-local samples=10 num_questions=5
+# python run.py -m model=llama3.1-405b-local samples=10 num_questions=5
+# python run_mimoracle.py -m model=llama3.1-405b-local samples=18 num_questions=5
