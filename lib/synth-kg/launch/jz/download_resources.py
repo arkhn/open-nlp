d51d2afb81f5f663d83347f62b79a0f81b73ec68
Simon Meoni <simonmeoni@aol.com>
2025-03-11 11:03:51 +0100

    ✨Synth-Kg: Add dl resources for jz

--- lib/synth-kg/launch/jz/download_resources.py
+++ lib/synth-kg/launch/jz/download_resources.py
@@ -1,3 +1,4 @@
+from sentence_transformers import SentenceTransformer
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 AutoModelForCausalLM.from_pretrained("meta-llama/llama-2-7b-hf")
@@ -5,3 +6,6 @@ AutoTokenizer.from_pretrained("meta-llama/llama-2-7b-hf")
 
 AutoModelForCausalLM.from_pretrained("xz97/AlpaCare-llama2-13b")
 AutoTokenizer.from_pretrained("xz97/AlpaCare-llama2-13b")
+
+SentenceTransformer("FremyCompany/BioLORD-2023")
+AutoTokenizer.from_pretrained("FremyCompany/BioLORD-2023")
